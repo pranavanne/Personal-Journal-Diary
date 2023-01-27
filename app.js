71fa31979aeae1da28a97b0aab28fc1a8a5716fd
pranavanne <pranav.anne21@gmail.com>
2023-01-27 21:24:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const mongoose = require("mongoose");
+const { urlencoded } = require("body-parser");
+mongoose.connect("mongodb+srv://admin-pranav:admin-pranav@cluster0.kwefnxq.mongodb.net/blogDB", {useNewUrlParser: true});
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+const Post = mongoose.model("Post", postSchema);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      posts: posts
+      });
+    }
+  )
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  })
+  });
+
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  Post.findById(requestedPostId, function(err, post){     // find() does not work!!
+    if(!err){
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+        postID: post._id
+      });
+    };
+  });
+});
+
+app.post("/delete/:postID", function(req, res){
+  const postId = req.params.postID;
+  Post.findByIdAndRemove(postId, function(err, post){
+    if(!err){
+      res.redirect("/");
+    }
+    else{
+      console.log(err);
+    }
+
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
